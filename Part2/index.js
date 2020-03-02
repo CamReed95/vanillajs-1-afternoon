48c30772bf3f771e627a211dfdfda2195117101e
Cameron Reed <cameronreedadjusting@gmail.com>
2020-03-02 16:48:59 -0700

    Completed part 2

--- Part2/index.js
+++ Part2/index.js
@@ -1,12 +1,27 @@
 console.log('The House always wins!');
 
+// let cardHearts = document.getElementsById('hearts');
+// let cardClubs = document.getElementsById('clubs');
+// let cardSpades = document.getElementsById('spades');
+// let cardDiamonds = document.getElementsById('diamonds';)
+
 let idInput = document.getElementById('idInput')
 let colorInput = document.getElementById('colorInput')
 
 function setCard() {
     let card = document.getElementById(idInput.value);
-    car.style.color = colorInput.value;
+    card.style.color = colorInput.value;
 }
+ function reset() {
+     let cards = document.getElementsByTagName('section') 
+     
+     cards[0].style.color = 'gray'
+     cards[1].style.color = 'gray'
+     cards[2].style.color = 'gray'
+     cards[3].style.color = 'gray'
+     
+ }
+
 
 
 
@@ -18,11 +33,6 @@ function setCard() {
 // - Target the card that is being selected in the function `setCard`.
 // - Using the information gathered from the second input field, add the `color` style to the targeted card element.
 //     - Assign the appropriate color to each card (Diamonds/Hearts = red, Clubs/Spades = black).
-
-// <details>
-// <summary>
-// <code>Detailed Instructions</code>
-// </summary>
 
 // - The final thing we are going to do is apply a color to the section using the second input we have set up.
 // - Using `card.style.color` set it equal to `colorInput.value`, being whatever is typed into the second input.
