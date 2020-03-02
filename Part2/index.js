abdea76b7ee3c4b675ef49c18397287060d5de79
Cameron Reed <cameronreedadjusting@gmail.com>
2020-03-02 15:03:10 -0700

    Progress on pt2

--- Part2/index.js
+++ Part2/index.js
@@ -1,2 +1,30 @@
 console.log('The House always wins!');
 
+let idInput = document.getElementById('idInput')
+let colorInput = document.getElementById('colorInput')
+
+function setCard() {
+    let card = document.getElementById(idInput.value);
+    car.style.color = colorInput.value;
+}
+
+
+
+
+
+
+
+// - Open `index.js`.
+// - Target the card that is being selected in the function `setCard`.
+// - Using the information gathered from the second input field, add the `color` style to the targeted card element.
+//     - Assign the appropriate color to each card (Diamonds/Hearts = red, Clubs/Spades = black).
+
+// <details>
+// <summary>
+// <code>Detailed Instructions</code>
+// </summary>
+
+// - The final thing we are going to do is apply a color to the section using the second input we have set up.
+// - Using `card.style.color` set it equal to `colorInput.value`, being whatever is typed into the second input.
+// - If done properly the text and symbol should change colors appropriately.
+
