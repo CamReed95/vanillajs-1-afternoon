52ba854b42c2cc723e3c82cc95aeb79a79152f02
Cameron Reed <cameronreedadjusting@gmail.com>
2020-03-09 16:17:17 -0700

    Completed VJS-1

--- Part1/index.js
+++ Part1/index.js
@@ -1,7 +1,3 @@
-
-
-const board = [];
-
 const board = [];
 
 function play(clickedId) {
@@ -28,6 +24,7 @@ function play(clickedId) {
   const bottomLeft = board[6];
   const bottomCenter = board[7];
   const bottomRight = board[8];
+
 
 
   if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
@@ -63,6 +60,8 @@ function play(clickedId) {
     return;
   }
 
+
+
   
   let boardFull = true;
   for (let i = 0; i <= 8; i++) {
@@ -74,89 +73,3 @@ function play(clickedId) {
     alert("Cat's game, there is no winner");
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let board = []
-
-
-// function play(clickedId) {
-//     let playerSpan = document.getElementById('player');
-//     let clickedElement = document.getElementById(clickedId);
-  
-//     if (playerSpan.innerText === 'X') {
-//       playerSpan.innerText = 'O';
-//       clickedElement.innerText = 'X';
-//       board[clickedId] = 'X';
-//     } else {
-//       playerSpan.innerText = 'X';
-//       clickedElement.innerText = 'O';
-//       board[clickedId] = 0;
-//     }
-  
-
-//     let topLeft = board[0];
-//     let topMiddle = board[1];
-//     let topRight = board[2];
-//     let middleLeft = board[3];
-//     let middleMiddle = board[4];
-//     let middleRight = board[5] 
-//     let bottomLeft = board[6] 
-//     let bottomMiddle = board[7]  
-//     let bottomRight = board[8] 
-
-//     if (topLeft !== undefined && topLeft === topMiddle && topLeft === topRight)
-//     alert(`${topLeft} is the winner`);
-//     return;
-
-//     if (topMiddle !== undefined && topMiddle === topLeft && topMiddle === topRight)
-//     alert(`${topMiddle} is the winner`);
-//     return;
-
-//     if (topRight !== undefined && topRight === topMiddle && topRight === topLeft)
-//     alert(`${topRight} is the winner`);
-//     return;
-
-//     if (middleLeft !== undefined && middleLeft === middleMiddle && middleLeft === middleRight)
-//     alert(`${middleLeft} is the winner`);
-//     return;
-
-//     if (middleMiddle !== undefined && middleMiddle === middleLeft && middleMiddle === middleRight)
-//     alert(`${middleMiddle} is the winner`)
-
-//     if (middleRight !== undefined && middleRight === middleMiddle && middleRight === middleLeft)
-//     alert(`${middleRight} is the winner`);
-//     return;
-
-//     if (bottomLeft !== undefined && bottomLeft === bottomMiddle && bottomLeft === bottomRight)
-//     alert(`${bottomLeft} is the winner`);
-//     return;
-
-//     if (bottomMiddle !== undefined && bottomMiddle === bottomLeft && bottomMiddle === bottomRight)
-//     alert(`${bottomMiddle} is the winner`);
-//     return;
-
-//     if (bottomRight !== undefined && bottomRight === bottomLeft && bottomRight === bottomMiddle)
-//     alert(`${bottomRight} is the winner`);
-//     return;
-
-//     let boardFull = true;
-//     for (let i = 0; i <= 8; i++) {
-//         if (board[i] === undefined) {
-//             boardFull = false
-//         } 
-//     }
-//         if (boardFull === true) {
-//             alert(`cat's game`);
-//             }
-// }
--- Part2/index.js
+++ Part2/index.js
@@ -1,40 +1,9 @@
 console.log('The House always wins!');
 
-// let cardHearts = document.getElementsById('hearts');
-// let cardClubs = document.getElementsById('clubs');
-// let cardSpades = document.getElementsById('spades');
-// let cardDiamonds = document.getElementsById('diamonds';)
-
-let idInput = document.getElementById('idInput')
-let colorInput = document.getElementById('colorInput')
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
 
 function setCard() {
-    let card = document.getElementById(idInput.value);
-    card.style.color = colorInput.value;
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
 }
- function reset() {
-     let cards = document.getElementsByTagName('section') 
-     
-     cards[0].style.color = 'gray'
-     cards[1].style.color = 'gray'
-     cards[2].style.color = 'gray'
-     cards[3].style.color = 'gray'
-     
- }
-
-
-
-
-
-
-
-
-// - Open `index.js`.
-// - Target the card that is being selected in the function `setCard`.
-// - Using the information gathered from the second input field, add the `color` style to the targeted card element.
-//     - Assign the appropriate color to each card (Diamonds/Hearts = red, Clubs/Spades = black).
-
-// - The final thing we are going to do is apply a color to the section using the second input we have set up.
-// - Using `card.style.color` set it equal to `colorInput.value`, being whatever is typed into the second input.
-// - If done properly the text and symbol should change colors appropriately.
-
